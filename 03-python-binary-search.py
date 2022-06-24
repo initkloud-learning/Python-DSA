800b347abd4d4a45f3d6c45377928871523dbc1d
PanwarAshish <panwar.ashish10@gmail.com>
2022-06-24 23:58:09 +0530

    chore: binary search.

--- 03-python-binary-search.py
+++ 03-python-binary-search.py
@@ -28,5 +28,50 @@ def locate_card(cards, query):
     return -1
 
 
-cards = [9, 7, 6, 4, 4, 4, 4, 3, 2, 1]
-print("Position:", locate_card(cards=cards, query=4))
+cards = [-1, -2, -3, -4, -5, -6]
+print("Position:", locate_card(cards=cards, query=-4))
+
+
+# --------------------------------------
+def binary_search(lo, hi, condition):
+    """TODO - add docs"""
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
+
+
+def first_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid > 0 and nums[mid-1] == target:
+                return 'left'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        else:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+
+
+def last_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid < len(nums)-1 and nums[mid+1] == target:
+                return 'right'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        else:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+
+
+def first_and_last_position(nums, target):
+    return first_position(nums, target), last_position(nums, target)
