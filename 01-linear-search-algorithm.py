b45687534125cfad69beefd1b6ecb0dba42d473c
PanwarAshish <panwar.ashish10@gmail.com>
2022-06-24 22:19:25 +0530

    chore: linear search.

---
+++ 01-linear-search-algorithm.py
@@ -0,0 +1,24 @@
+# Linear Search Algorithm
+
+def locate_card(cards, query):
+    position = 0
+
+    while True:
+        if cards[position] == query:
+            return position
+        position += 1
+
+        if position == len(cards):
+            return -1
+
+
+def locate_card02(cards, query):
+    position = 0
+
+    while position < len(cards):
+        if cards[position] == query:
+            return position
+        position += 1
+
+        if position == len(cards):
+            return -1
